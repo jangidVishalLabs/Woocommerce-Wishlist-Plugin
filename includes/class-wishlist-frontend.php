28ad3ed553f1a1203671f79243a87a4bbbf76261
jangidVishalLabs <vishal.jangid@wisdmlabs.com>
2026-01-20 12:21:04 +0530

    Design button and hook button in shop and product pages

---
+++ includes/class-wishlist-frontend.php
@@ -0,0 +1,32 @@
+<?php
+if( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+class WC_Wishlist_frontend {
+
+	public static function render_button( $product ){
+		if ( ! $product instanceof WC_Product ) {
+			return;
+		}
+
+		$user_id = get_current_user_id();
+		$product_id = $product->get_id();
+		$variation_id = 0;
+
+		$is_logged_in = is_user_logged_in();
+		$in_washlist = false;
+		if ( $is_logged_in ) {
+			$in_washlist = WC_Wishlist_DB::is_in_wishlist( $user_id, $product_id, $variation_id );
+		}
+
+		$button_text = $in_wishlist ? 'Wishlisted' : 'Add to Wishlist';
+		$button_class = $in_wishlist ? 'added' : '';
+
+		?>
+		<button class="wc_wishlist_btn <?php echo esc_attr( $button_class ); ?>" data-product-id="<?php echo esc_attr( $product_id ); ?>" data-variation-id="<?php echo esc_attr( $variation_id ); ?>" data-logged-in="<?php echo  $is_logged_in ? "1" : "0"; ?>">❤️ <?php echo esc_html( $button_text ); ?></button>
+		
+		
+		<?php
+	}
+}
--- wishlist-plugin.php
+++ wishlist-plugin.php
@@ -14,9 +14,24 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-wishlist-db.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-wishlist-frontend.php';
 
 register_activation_hook( __FILE__, 'wc_wishlist_plugin_activate' );
 
 function wc_wishlist_plugin_activate() {
 	WC_Wishlist_DB::create_table();
+}
+
+add_action( 'woocommerce_after_shop_loop_item', 'wc_wishlist_render_button', 20 );
+function wc_wishlist_render_button() {
+	global $product;
+
+	WC_Wishlist_frontend::render_button( $product );
+}
+
+add_action('woocommerce_single_product_summary', 'wc_wishlist_single_button', 35);
+function wc_wishlist_single_button() {
+	global $product;
+
+	WC_Wishlist_frontend::render_button( $product );
 }
